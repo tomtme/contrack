dd884e779c1a56753d57b6c56abb35869229a109
Tom Tijerina <tom@tomt.me>
2016-01-01 15:41:23 -0500

    try again

--- controller/pagecontroller.php
+++ controller/pagecontroller.php
@@ -25,9 +25,10 @@ class PageController extends Controller {
 
 	private $userId;
 
-	public function __construct($AppName, IRequest $request, $UserId){
+	public function __construct($AppName, IRequest $request, TestMapper $mapper, $UserId){
 		parent::__construct($AppName, $request);
 		$this->userId = $UserId;
+		$this->mapper = $mapper;
 	}
 
 	/**
